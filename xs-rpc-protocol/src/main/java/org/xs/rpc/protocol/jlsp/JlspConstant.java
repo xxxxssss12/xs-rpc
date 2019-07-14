a379650081ab6d29ec4bf468d22369443628b2f3
xxxxssss12 <xs6666@qq.com>
2019-07-14 22:06:03 +0800

    ok

---
+++ xs-rpc-protocol/src/main/java/org/xs/rpc/protocol/jlsp/JlspConstant.java
@@ -0,0 +1,12 @@
+package org.xs.rpc.protocol.jlsp;
+
+/**
+ * just line seperator protocal
+ *
+ * create by xs
+ * create time:2019-07-14 11:45:56
+ */
+public class JlspConstant {
+
+    public static byte[] SEPERATOR = new byte[]{0x01, 0x1A, 0x01, 0x1A};
+}
