75aa1f3ffa0c2fe42e9bcffc3f583bdb44f070a8
xxxxssss12 <xs6666@qq.com>
2019-11-14 10:06:55 +0800

    cmt

---
+++ xs-rpc-config/src/main/java/org/xs/rpc/config/RegistryConfig.java
@@ -0,0 +1,13 @@
+package org.xs.rpc.config;
+
+import java.net.Inet4Address;
+
+/**
+ * create by xs
+ * create time:2019-11-14 09:20:22
+ */
+public class RegistryConfig {
+    private String protocol;    // 协议
+    private String hostName;    //
+    private Integer port;
+}
